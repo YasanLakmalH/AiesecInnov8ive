37ffe30d6b61db094c2f2cdb436cb10f5b8c2f11
YasanLakmalH <yasanhemachandra@gmail.com>
2024-06-19 15:47:06 +0530

    daisy Ui Configured.

---
+++ client/app/welcome/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+<div className='flex justify-center items-center mt-40'>
+      this is welcome page.
+    </div>
+  )
+}
+
+export default page
