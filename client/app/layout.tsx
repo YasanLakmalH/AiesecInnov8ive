cb583e8fe71c641dc484999e9686701a7b3c4454
Danindu Chamoth <154650297+Danindu-Chamoth@users.noreply.github.com>
2024-06-20 11:10:06 +0530

    fix some isuses and create new navbar file and footer files

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-
+import Navbar from "./Navbar";
+import Footer from "./footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+        <Navbar/>
         {children}
         <Footer/>
         </body>
