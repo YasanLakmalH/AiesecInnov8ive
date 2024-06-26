f382d61a7e574513db2599a38a0bf0e9aa5eb070
Danindu Chamoth <154650297+Danindu-Chamoth@users.noreply.github.com>
2024-06-26 09:01:29 +0530

    some changes

--- client/app/Dashboard/page.tsx
+++ client/app/Dashboard/page.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import { options } from '../api/auth/[...nextauth]/options';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
+
 
 const Page = async () => {
-    const session = await getServerSession(options);
-
-    if(!session){
-        redirect('/api/auth/signin?callbackUrl=/dashboard');
-    }
+    
   return (
     <div>
       <h1>Dashboard</h1>
